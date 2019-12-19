97220be014521bdfb3e6d779e9645be9fad2661f
Vicillian <vic.liu101@gmail.com>
2019-12-19 02:21:37 +0000

    When button click it becme X or o depending on who's turn it is.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@ var User1 = "X";
 var turnsTaken = 0;
 var gameEnd = false;
 
-function playerturn(){
-if()
+function playerturn(buttonId){
+if(User1 === "X"){
+ User1 = "O" 
+}else{
+  User1 = "X"
+}
+};
+
+function takenTurns(x){
+  turnsTaken + x
 }
 
 function performLogic(buttonId, tileId){
@@ -15,40 +23,49 @@ function performLogic(buttonId, tileId){
     $(tileId).html(User1);
 }
 
-$("#button1").click(function() {
+$('#button1').click(function() {
     performLogic("#button1","#tile1");
-
+  console.log();
+    playerturn("#button1");
 });
 
 $("#button2").click(function() {
     performLogic("#button2","#tile2");
+  playerturn("#button2");
 });
 
 $("#button3").click(function() {
     performLogic("#button3","#tile3");
+  playerturn("#button3");
 });
 
 $("#button4").click(function() {
     performLogic("#button4","#tile4");
+  playerturn("#button4");
 });
 
 $("#button5").click(function() {
     performLogic("#button5","#tile5");
+  playerturn("#button5");
 });
 
 $("#button6").click(function() {
     performLogic("#button6","#tile6");
+  playerturn("#button6");
 });
 
 $("#button7").click(function() {
     performLogic("#button7","#tile7");
+  playerturn("#button7");
 });
 
 $("#button8").click(function() {
     performLogic("#button8","#tile8");
+  playerturn("#button8");
 });
 
 $("#button9").click(function() {
     performLogic("#button9","#tile9");
+  playerturn("#button9");
 });
 
